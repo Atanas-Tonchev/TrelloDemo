39bc6c788e0dfb79c87d17de2c56447eab8220f7
AtanasTonchevRms <atanas.tonchev@rms.com>
2025-09-30 02:05:51 +0300

    -

--- src/main/java/tests/BoardSetupTest.java
+++ src/main/java/tests/BoardSetupTest.java
@@ -1,11 +1,12 @@
 package tests;
 
 import config.BaseTest;
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 public class BoardSetupTest extends BaseTest {
 
-@Test()
+@Test (groups = {"BoardSetup"},
+    priority = 1)
   public void boardSetupTest() {
     logger.info("Starting Board Setup Test");
     // Test implementation goes here
